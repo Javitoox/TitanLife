c9de1f3481f0ed541de8cf8396e3fbd47e3aa904
almaf <almaf@LAPTOP-2N5E111J>
2020-04-18 20:44:29 +0200

    PEPE

--- Proyect-TitanLife/src/main/java/aiss/model/User.java
+++ Proyect-TitanLife/src/main/java/aiss/model/User.java
@@ -1,99 +1,97 @@
 package aiss.model;
 
 public class User {
-	
-	private String username;
-	private String email;
-	private String password;
-	private String retype;
-	private Integer age;
-	private Integer height;
-	private Integer weight;
-	private Integer hip;
-	private Integer waist;
-	
-	public User(String username, String email, String password, String retype, Integer age, Integer height, Integer weight, Integer hip, Integer waist) {
-		this.username=username;
-		this.email=email;
-		this.password=password;
-		this.retype=retype;
-		this.age=age;
-		this.height=height;
-		this.weight=weight;
-		this.hip=hip;
-		this.waist=waist;
-	}
+    private String username;
+    private String email;
+    private String password;
+    private String retype;
+    private Integer age;
+    private Integer height;
+    private Integer weight;
+    private Integer hip;
+    private Integer waist;
 
-	public String getUsername() {
-		return username;
-	}
+    public User(String username, String email, String password, String retype, Integer age, Integer height, Integer weight, Integer hip, Integer waist) {
+        this.username=username;
+        this.email=email;
+        this.password=password;
+        this.retype=retype;
+        this.age=age;
+        this.height=height;
+        this.weight=weight;
+        this.hip=hip;
+        this.waist=waist;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public String getRetype() {
-		return retype;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setRetype(String retype) {
-		this.retype = retype;
-	}
+    public String getRetype() {
+        return retype;
+    }
 
-	public Integer getAge() {
-		return age;
-	}
+    public void setRetype(String retype) {
+        this.retype = retype;
+    }
 
-	public void setAge(Integer age) {
-		this.age = age;
-	}
+    public Integer getAge() {
+        return age;
+    }
 
-	public Integer getHeight() {
-		return height;
-	}
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
-	public void setHeight(Integer height) {
-		this.height = height;
-	}
+    public Integer getHeight() {
+        return height;
+    }
 
-	public Integer getWeight() {
-		return weight;
-	}
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
 
-	public void setWeight(Integer weight) {
-		this.weight = weight;
-	}
+    public Integer getWeight() {
+        return weight;
+    }
 
-	public Integer getHip() {
-		return hip;
-	}
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
 
-	public void setHip(Integer hip) {
-		this.hip = hip;
-	}
+    public Integer getHip() {
+        return hip;
+    }
 
-	public Integer getWaist() {
-		return waist;
-	}
+    public void setHip(Integer hip) {
+        this.hip = hip;
+    }
 
-	public void setWaist(Integer waist) {
-		this.waist = waist;
-	}
-	
+    public Integer getWaist() {
+        return waist;
+    }
+
+    public void setWaist(Integer waist) {
+        this.waist = waist;
+    }
 }
--- Proyect-TitanLife/src/main/java/aiss/model/repository/UserRepository.java
+++ Proyect-TitanLife/src/main/java/aiss/model/repository/UserRepository.java
@@ -8,57 +8,54 @@ import java.util.logging.Logger;
 import aiss.model.User;
 
 public class UserRepository {
+    private static final Logger log = Logger.getLogger(UserRepository.class.getName());
 
-	private static final Logger log = Logger.getLogger(UserRepository.class.getName());
+    private Map<String,User> users;
+    private static UserRepository instance=null;
 
-	private Map<String,User> users;
-	private static UserRepository instance=null;
-	
-	public static UserRepository getInstance() {
-		
-		if (instance==null) {
-			instance = new UserRepository();
-			instance.init();
-		}
-		
-		return instance;
-	}
-	
-	public void init() {
-		users = new HashMap<String,User>();
-	}
+    public static UserRepository getInstance() {
 
-	public Map<String,User> getUsers() {
-		Map<String,User> us= users;
-		log.log(Level.INFO, "La lista de usuarios " + us + " fue obtenida");
-		return us;
-	}
+        if (instance==null) {
+            instance = new UserRepository();
+            instance.init();
+        }
 
-	public void updateUser(User u) {
-		users.put(u.getUsername(), u);
-		log.log(Level.INFO, "El usuario " + u.getUsername() + " fue actualizado");
+        return instance;
+    }
 
-	}
+    public void init() {
+        users = new HashMap<String,User>();
+    }
 
-	public User getUser(String un) {
-		User u =users.get(un);
-		if(u!=null)
-			log.log(Level.INFO, "El usuario " + u.getUsername() + " fue obtenido");
-		return u;
-	}
+    public Map<String,User> getUsers() {
+        Map<String,User> us= users;
+        log.log(Level.INFO, "La lista de usuarios " + us + " fue obtenida");
+        return us;
+    }
 
-	public User addUser(String username, String email, String password, String retype, Integer age, Integer height, Integer weight, Integer hip, Integer waist) {
-		User u = new User(username,email,password,retype,age,height,weight,hip,waist);
-		users.put(username,u);
-		log.log(Level.INFO, "El usuario " + u.getUsername() + " fue creado");
-		return u;
-	}
+    public void updateUser(User u) {
+        users.put(u.getUsername(), u);
+        log.log(Level.INFO, "El usuario " + u.getUsername() + " fue actualizado");
 
-	public void deleteContact(String un) {
-		User u=users.get(un);
-		users.remove(un);
-		log.log(Level.INFO, "El usuario " + u.getUsername() + " fue eliminado");
-	}
+    }
 
+    public User getUser(String un) {
+        User u =users.get(un);
+        if(u!=null)
+            log.log(Level.INFO, "El usuario " + u.getUsername() + " fue obtenido");
+        return u;
+    }
+
+    public User addUser(String username, String email, String password, String retype, Integer age, Integer height, Integer weight, Integer hip, Integer waist) {
+        User u = new User(username,email,password,retype,age,height,weight,hip,waist);
+        users.put(username,u);
+        log.log(Level.INFO, "El usuario " + u.getUsername() + " fue creado");
+        return u;
+    }
+
+    public void deleteContact(String un) {
+        User u=users.get(un);
+        users.remove(un);
+        log.log(Level.INFO, "El usuario " + u.getUsername() + " fue eliminado");
+    }
 }
-
