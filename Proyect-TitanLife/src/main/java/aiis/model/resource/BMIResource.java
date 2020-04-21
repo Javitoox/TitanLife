04a0f8476aa2d5b2f92d0f037c7b4f34b36c9225
Javitoox <javikuka7@gmail.com>
2020-04-21 14:09:16 +0200

    .

--- Proyect-TitanLife/src/main/java/aiis/model/resource/BMIResource.java
+++ Proyect-TitanLife/src/main/java/aiis/model/resource/BMIResource.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 public class BMIResource {
 	private static final Logger log = Logger.getLogger(BMIResource.class.getName());
-    private final String uri = "https://www.googleapis.com/drive/v2/files";
+    private final String uri = "https://www.bmi.p.rapidapi.com";
     
     
 
